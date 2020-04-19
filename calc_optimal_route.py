a230925a24c15431b614e2040831001e57b62bde
FKoncz <fkoncz@outlook.com>
2020-04-18 22:56:13 -0400

    Initial commit

---
+++ calc_optimal_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+PLEASE CONTACT ME AT MY E-MAIL - FKONCZ@OUTLOOK.COM - FOR THE FULLY WORKING SOLUTION!
+"""
+
+
+def main():
+    return
+
+
+if __name__ == "main":
+    main()
